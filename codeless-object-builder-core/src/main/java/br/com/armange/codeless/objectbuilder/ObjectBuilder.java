f417616bc17966dbc1ac8116f86bb2e7dc3447b2
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-03-25 16:05:18 -0300

    CSV Reader.

--- codeless-object-builder-core/src/main/java/br/com/armange/codeless/objectbuilder/ObjectBuilder.java
+++ codeless-object-builder-core/src/main/java/br/com/armange/codeless/objectbuilder/ObjectBuilder.java
@@ -1,6 +1,12 @@
 package br.com.armange.codeless.objectbuilder;
 
+import java.io.InputStream;
+
 public class ObjectBuilder {
 
     private ObjectBuilder() {}
+    
+    public Object fromInputStream(final InputStream inputStream) {
+        return new InputStreamReader(inputStream);
+    }
 }
