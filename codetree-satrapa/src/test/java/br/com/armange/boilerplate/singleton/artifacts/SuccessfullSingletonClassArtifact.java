5d0eebc62032c13ed56309dc564152688ea245ec
Diego Armange Costa <diego.armange.costa@gmail.com>
2019-10-22 23:16:09 -0300

    Initial commit

---
+++ codetree-satrapa/src/test/java/br/com/armange/boilerplate/singleton/artifacts/SuccessfullSingletonClassArtifact.java
@@ -0,0 +1,6 @@
+package br.com.armange.boilerplate.singleton.artifacts;
+
+import br.com.armange.boilerplate.singleton.annotation.Singleton;
+
+@Singleton
+public class SuccessfullSingletonClassArtifact {}
